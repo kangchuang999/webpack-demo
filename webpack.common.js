bd18e538bd7d9eb1546d1a12a741834614bb79ea
kangchuang <v_kangchuang@baidu.com>
2017-11-24 14:56:10 +0800

    add new function

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,31 @@
   const path = require('path');
   const CleanWebpackPlugin = require('clean-webpack-plugin');
   const HtmlWebpackPlugin = require('html-webpack-plugin');
- 
+  const webpack = require('webpack');
+
   module.exports = {
-    entry: {
-      app: './src/index.js'
+    entry: { // 入口文件可以有多个  出口只有一个，但不代表只有一个出口文件；
+      main: './src/index.js',
+      vendor: [
+        'lodash'
+      ]
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: '[name].bundle.js',
-      chunkFilename: '[name].bundle.js',
-      publicPath: '/'
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[name].bundle.js', // 例如异步调用的代码块（非必需的）
+      // publicPath: '/'
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Production'
+      new HtmlWebpackPlugin({  // 在产出目录生成一个html
+        title: 'caching'
       }),
-      new CleanWebpackPlugin(['./dist'])
+      new CleanWebpackPlugin(['./dist']), // 删除产出目录
+      new webpack.optimize.CommonsChunkPlugin({ // 提取引入的公用代码块
+        name: 'vendor'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({ // 提取webpack自带的代码块
+        name: 'runtime'
+      })
     ]
   };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,15 @@
   const merge = require('webpack-merge');
   const common = require('./webpack.common.js');
- 
+  const webpack = require('webpack');
+
   module.exports = merge(common, {
-    devtool: 'inline-source-map',
-    // devServer: {
+    devtool: 'inline-source-map', // 知道错误在那个文件；
+    // devServer: {  // 配合 webpack-dev-server使用；
     //   contentBase: './dist'
     // }
+    plugins: [
+      // new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('development') }), // 告诉webpack解压结果用于什么环境
+      new webpack.HashedModuleIdsPlugin(), // 使webpack.optimize.CommonsChunkPlugin提取出来的模块hash不变；
+    ]
   });
